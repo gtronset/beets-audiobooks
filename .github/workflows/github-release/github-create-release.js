ff41e4bdc9b3efe6f0916b61e61aefb67a7f22e7
Gavin Tronset <gtronset@gmail.com>
2023-05-10 11:29:55 -0700

    Refactor Workflows

---
+++ .github/workflows/github-release/github-create-release.js
@@ -0,0 +1,44 @@
+module.exports = async ({github, context, core}) => {
+    try {
+
+        const {owner, repo} = context.repo;
+        const {NEW_BA_VERSION, PROJECT_VERSION, LSIO_VERSION, TAG_NAME} = process.env;
+
+        const generated_notes_response = await github.rest.repos.generateReleaseNotes({
+            owner: owner,
+            repo: repo,
+            tag_name: TAG_NAME,
+        });
+
+        const generated_release_notes = generated_notes_response.data.body
+
+        const new_release_notes = `# Release Notes (${TAG_NAME})\n\n` +
+            `## Version Info\n` +
+            `* [Beets](https://github.com/beetbox/beets): \`v${PROJECT_VERSION}\`\n` +
+            `* [LinuxServer \`docker-beets\`](https://github.com/linuxserver/docker-beets):` +
+            ` \`${PROJECT_VERSION}-ls${LSIO_VERSION}\`\n` +
+            `* \`beets-audiobook\` release: \`${NEW_BA_VERSION}\`` +
+            `\n\n` +
+            generated_release_notes
+
+        core.exportVariable('RELEASE_NOTES', new_release_notes);
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+
+
+    // try {
+    //     await github.rest.repos.createRelease({
+    //         draft: false,
+    //         generate_release_notes: true,
+    //         name: process.env.RELEASE_TAG,
+    //         owner: context.repo.owner,
+    //         prerelease: false,
+    //         repo: context.repo.repo,
+    //         tag_name: process.env.RELEASE_TAG,
+    //     });
+    // } catch (error) {
+    //     core.setFailed(error.message);
+    // }
+}
--- .github/workflows/github-release/github-release-check.js
+++ .github/workflows/github-release/github-release-check.js
@@ -1,10 +1,9 @@
 module.exports = async ({github, context, core}) => {
-        try {
+    try {
 
         const {owner, repo} = context.repo;
-        const {PROJECT_VERSION, LSIO_VERSION, BA_VERSION} = process.env;
+        const {PROJECT_VERSION, BA_VERSION} = process.env;
 
-        console.log(PROJECT_VERSION, LSIO_VERSION, BA_VERSION);
         const latest_release_response = await github.rest.repos.getLatestRelease({
             owner: owner,
             repo: repo,
@@ -29,6 +28,7 @@ module.exports = async ({github, context, core}) => {
         }
 
         core.exportVariable('RELEASE_NEEDED', true);
+        core.exportVariable('OLD_TAG_NAME', latest_release_tag_name);
 
         console.log("Commits since last release: ", total_commits);
 
@@ -39,44 +39,10 @@ module.exports = async ({github, context, core}) => {
         const new_tag_name = `v${PROJECT_VERSION}-ba${new_ba_version}`;
         console.log("New release tag: ", new_tag_name);
 
-
         core.exportVariable('TAG_NAME', new_tag_name);
-
-        const generated_notes_response = await github.rest.repos.generateReleaseNotes({
-            owner: owner,
-            repo: repo,
-            tag_name: new_tag_name,
-        });
-
-        const generated_release_notes = generated_notes_response.data.body
-
-        const new_release_notes = `# Release Notes (${new_tag_name})\n\n` +
-            `## Version Info\n` +
-            `* [Beets](https://github.com/beetbox/beets): \`v${PROJECT_VERSION}\`\n` +
-            `* [LinuxServer \`docker-beets\`](https://github.com/linuxserver/docker-beets):` +
-            ` \`${PROJECT_VERSION}-ls${LSIO_VERSION}\`\n` +
-            `* \`beets-audiobook\` release: \`${new_ba_version}\`` +
-            `\n\n` +
-            generated_release_notes
-
-        core.exportVariable('RELEASE_NOTES', new_release_notes);
 
     } catch (error) {
         core.setFailed(error.message);
     }
 
-
-    // try {
-    //     await github.rest.repos.createRelease({
-    //         draft: false,
-    //         generate_release_notes: true,
-    //         name: process.env.RELEASE_TAG,
-    //         owner: context.repo.owner,
-    //         prerelease: false,
-    //         repo: context.repo.repo,
-    //         tag_name: process.env.RELEASE_TAG,
-    //     });
-    // } catch (error) {
-    //     core.setFailed(error.message);
-    // }
 }
