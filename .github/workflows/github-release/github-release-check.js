4f96791cec899f823cdf42052cb0dca34983e9f7
Gavin Tronset <gtronset@gmail.com>
2023-05-10 14:31:05 -0700

    Use github-script for yaml interaction

--- .github/workflows/github-release/github-release-check.js
+++ .github/workflows/github-release/github-release-check.js
@@ -1,8 +1,18 @@
+const yaml = require('js-yaml');
+const fs   = require('fs');
+
 module.exports = async ({github, context, core}) => {
     try {
 
         const {owner, repo} = context.repo;
-        const {PROJECT_VERSION, BA_VERSION} = process.env;
+        const versionFile = "repo-vars.yaml";
+
+        const yamlContents = yaml.load(fs.readFileSync(versionFile, 'utf8'));
+        const {project_version, ba_version, lsio_version} = yamlContents;
+
+        /* Make available the following variables in subsequent step(s) */
+        core.exportVariable('PROJECT_VERSION', project_version);
+        core.exportVariable('LSIO_VERSION', lsio_version);
 
         const latest_release_response = await github.rest.repos.getLatestRelease({
             owner: owner,
@@ -32,14 +42,23 @@ module.exports = async ({github, context, core}) => {
 
         console.log("Commits since last release: ", total_commits);
 
-        const new_ba_version = parseInt(BA_VERSION) + 1;
+        const new_ba_version = parseInt(ba_version) + 1;
 
         core.exportVariable('NEW_BA_VERSION', new_ba_version);
 
-        const new_tag_name = `v${PROJECT_VERSION}-ba${new_ba_version}`;
+        const new_tag_name = `v${project_version}-ba${new_ba_version}`;
         console.log("New release tag: ", new_tag_name);
 
         core.exportVariable('TAG_NAME', new_tag_name);
+
+
+        /* Update the Version File */
+        yamlContents.ba_version = new_ba_version
+        yamlContents.previous_release = latest_release_tag_name
+        yamlContents.current_release = new_tag_name
+        fs.writeFileSync(versionFile, yaml.dump(yamlContents));
+
+        console.log(yamlContents);
 
     } catch (error) {
         core.setFailed(error.message);
